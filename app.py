0d83612ed37d837a33e481b9502fa4631bc8a441
ericchen1972 <power.windows@msa.hinet.net>
2025-04-16 22:56:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import os
 class InferlessPythonModel:    
     def initialize(self):
         nltk.download('averaged_perceptron_tagger_eng')
-        self.model = TTS(language='EN', device='auto')
+        self.model = TTS(language='ZH', device='auto')
         self.speaker_ids = self.model.hps.data.spk2id
         self.output_path = 'temp.wav'
         
     def infer(self, inputs):
         text = inputs["text"]
-        self.model.tts_to_file(text, self.speaker_ids['EN-US'], self.output_path, speed=0.7)
+        self.model.tts_to_file(text, self.speaker_ids['ZH-CN-Yunzhe'], self.output_path, speed=0.7)
         
         with open(self.output_path, 'rb') as file:
             audio_data = file.read()
@@ -21,8 +21,7 @@ class InferlessPythonModel:
             
         os.remove(self.output_path)
         return {
-            "generated_audio_base64":base64_message,
-            "speaker_ids": dict(self.speaker_ids)
+            "generated_audio_base64":base64_message
         }
 
     def finalize(self,args):
